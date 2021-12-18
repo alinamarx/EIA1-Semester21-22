3e85957e7ac8fd0fc9296d98b4ac4e06cc1d7b7b
alinamarx <alina.marx@hs-furtwangen.de>
2021-12-18 17:18:33 +0100

    Aufgabe 10 (vorläufig)

---
+++ 10/script.js
@@ -0,0 +1,136 @@
+var L09;
+(function (L09) {
+    window.addEventListener("load", function () {
+        // Umwandeln der HTMLElemente in Variabeln
+        var total = document.getElementById("total");
+        var newtask = document.querySelector("input");
+        var tasks = document.querySelector("ul");
+        var open = document.getElementById("open");
+        var done = document.getElementById("done");
+        //Variablendeklaration
+        var indexTotal = 0;
+        var indexOpen = 0;
+        var indexDone = 0;
+        var checked = false;
+        var microphoneClicked = false;
+        //Übergabe des Inputs an neue Variable, um dann Textfeld zu leeren
+        var textInput;
+        window.addEventListener("keydown", function (event) {
+            if (event.keyCode == 13)
+                addingTask(textInput);
+        });
+        function Counter(indexTotal, indexOpen, indexDone) {
+            total.innerHTML = indexTotal + " in total";
+            open.innerHTML = indexOpen + " open";
+            done.innerHTML = ", " + indexDone + " done";
+        }
+        //Ausführung der Funktion, wenn man etwas eingibt und 'Enter' drückt
+        function addingTask(input) {
+            indexTotal++;
+            indexOpen++;
+            Counter(indexTotal, indexOpen, indexDone);
+            if (newtask.value != "") {
+                input = newtask.value;
+                newtask.value = "";
+            }
+            //Erstellen der Listenelemente und Icons im DOM
+            var li = document.createElement("li");
+            var circle = document.createElement("i");
+            circle.classList.add("far");
+            li.appendChild(circle);
+            circle.classList.add("fa-circle");
+            li.appendChild(circle);
+            circle.addEventListener("click", checkingTask);
+            var checkmark = document.createElement("i");
+            checkmark.setAttribute("class", "fas fa-check");
+            checkmark.setAttribute("style", "opacity: 0");
+            li.appendChild(checkmark);
+            var taskString = document.createElement("span");
+            li.appendChild(taskString);
+            taskString.textContent = "" + input;
+            console.log(taskString);
+            var trash = document.createElement("i");
+            trash.classList.add("far");
+            trash.classList.add("fa-trash-alt");
+            li.appendChild(trash);
+            li.insertBefore(trash, li.childNodes[2]);
+            trash.addEventListener("click", deletingTask);
+            tasks.appendChild(li);
+            //Task wird bei Klick gecheckt und entcheckt
+            function checkingTask() {
+                if (!checked) {
+                    checkmark.setAttribute("style", "opacity: 1");
+                    indexOpen--;
+                    indexDone++;
+                    Counter(indexTotal, indexOpen, indexDone);
+                    checked = true;
+                }
+                else {
+                    checkmark.setAttribute("style", "opacity: 0");
+                    indexOpen++;
+                    indexDone--;
+                    Counter(indexTotal, indexOpen, indexDone);
+                    checked = false;
+                }
+            }
+            //Task wird gelöscht, total counter -1
+            function deletingTask() {
+                li.remove();
+                indexTotal--;
+                if (!checked) { //Die Task wird gelöscht, wenn sie nicht abgehakt ist -> -1 open
+                    indexOpen--;
+                }
+                else { //Die Task wird gelöscht, wenn sie abgehakt ist -> -1 done
+                    indexDone--;
+                }
+                Counter(indexTotal, indexOpen, indexDone);
+            }
+        }
+        //Spracheingabe mit Artyom
+        var artyom = new Artyom();
+        artyom.addCommands([{
+                indexes: ["erstelle Aufgabe *", "aufgabe erstellen *"],
+                smart: true,
+                action: function (i, voiceInput) {
+                    console.log("Neue Aufgabe wird erstellt: " + voiceInput);
+                    artyom.say("Neue Aufgabe wird erstellt: " + voiceInput);
+                    addingTask(voiceInput);
+                }
+            }, {
+                indexes: ["Lösche alle Aufgaben", "Lösche", "Löschen"],
+                smart: false,
+                action: function () {
+                    tasks.innerHTML = "";
+                    artyom.say("Alle Aufgaben wurden gelöscht");
+                }
+            }]);
+        function startContinuousArtyom() {
+            artyom.fatality();
+            setTimeout(function () {
+                artyom.initialize({
+                    lang: "de-DE",
+                    continuous: true,
+                    listen: true,
+                    interimResults: true,
+                    debug: true,
+                    speed: 0.8
+                }).then(function () {
+                    console.log("Ready!");
+                });
+            }, 250);
+        }
+        //Funktion "Record"-Button (Artyom hört zu/ hört auf zuzuhören) 
+        document.querySelector(".fa-microphone-alt").addEventListener("click", function () {
+            if (!microphoneClicked) {
+                startContinuousArtyom();
+                microphoneClicked = true;
+            }
+            else {
+                artyom.fatality();
+                console.log("Not ready!");
+                microphoneClicked = false;
+            }
+        });
+    });
+})(L09 || (L09 = {}));
+//# sourceMappingURL=script.js.map
---
+++ 10/script.ts
@@ -0,0 +1,153 @@
+namespace L09 {
+// tslint:disable: no-any
+declare var Artyom: any;
+
+window.addEventListener("load", function(): void {
+    // Umwandeln der HTMLElemente in Variabeln
+    let total: HTMLSpanElement = document.getElementById("total");
+    let newtask: HTMLInputElement = document.querySelector("input");
+    let tasks: HTMLElement = document.querySelector("ul");
+    let open: HTMLSpanElement = document.getElementById("open");
+    let done: HTMLSpanElement = document.getElementById("done");
+
+    //Variablendeklaration
+    let indexTotal: number = 0;
+    let indexOpen: number = 0;
+    let indexDone: number = 0;
+    let checked: boolean = false;
+    let microphoneClicked: boolean = false;
+
+    //Übergabe des Inputs an neue Variable, um dann Textfeld zu leeren
+    let textInput: string;
+
+    window.addEventListener("keydown", function(event: KeyboardEvent): void {
+        if (event.keyCode == 13) addingTask(textInput);
+    });
+
+    function Counter(indexTotal: number, indexOpen: number, indexDone: number): void {
+        total.innerHTML = indexTotal + " in total";
+        open.innerHTML = indexOpen + " open";
+        done.innerHTML = ", " + indexDone + " done";
+    }
+    
+    //Ausführung der Funktion, wenn man etwas eingibt und 'Enter' drückt
+    function addingTask (input: string): void {
+
+        indexTotal++;
+        indexOpen++;
+        Counter(indexTotal, indexOpen, indexDone);  
+    
+        if (newtask.value != "") {
+            input = newtask.value;
+            newtask.value = "";
+        } 
+         
+        //Erstellen der Listenelemente und Icons im DOM
+        const li: HTMLLIElement = document.createElement("li");
+        
+        const circle: HTMLElement = document.createElement("i");
+        circle.classList.add("far");
+        li.appendChild(circle);
+        circle.classList.add("fa-circle");
+        li.appendChild(circle);
+        circle.addEventListener("click", checkingTask);
+
+        const checkmark: HTMLElement = document.createElement("i");
+        checkmark.setAttribute("class", "fas fa-check");
+        checkmark.setAttribute("style", "opacity: 0");
+        li.appendChild(checkmark); 
+
+        const taskString: HTMLSpanElement = document.createElement("span");
+        li.appendChild(taskString);
+        taskString.textContent = "" + input;
+        console.log(taskString);
+
+        const trash: HTMLElement = document.createElement("i");
+        trash.classList.add("far");
+        trash.classList.add("fa-trash-alt");
+        li.appendChild(trash);
+        li.insertBefore(trash, li.childNodes[2]); 
+        trash.addEventListener("click", deletingTask);
+
+        tasks.appendChild(li);
+
+        //Task wird bei Klick gecheckt und entcheckt
+        function checkingTask (): void {
+            if (!checked) {
+                checkmark.setAttribute("style", "opacity: 1");
+                indexOpen--;
+                indexDone++;
+                Counter(indexTotal, indexOpen, indexDone);
+                checked = true;
+            } else {
+                checkmark.setAttribute("style", "opacity: 0");
+                indexOpen++;
+                indexDone--;
+                Counter(indexTotal, indexOpen, indexDone);
+                checked = false;
+            }
+        }
+
+        //Task wird gelöscht, total counter -1
+        function deletingTask (): void {
+            li.remove(); 
+            indexTotal--;
+            if (!checked) {   //Die Task wird gelöscht, wenn sie nicht abgehakt ist -> -1 open
+                indexOpen--;
+            } else {          //Die Task wird gelöscht, wenn sie abgehakt ist -> -1 done
+                indexDone--;
+            }
+            Counter(indexTotal, indexOpen, indexDone);
+        }    
+    }
+
+    //Spracheingabe mit Artyom
+    const artyom: any = new Artyom();
+    
+    artyom.addCommands([{
+        indexes: ["erstelle Aufgabe *", "aufgabe erstellen *"],
+        smart: true,
+        action: function(i: any, voiceInput: string): void {
+            console.log("Neue Aufgabe wird erstellt: " + voiceInput);
+            artyom.say("Neue Aufgabe wird erstellt: " + voiceInput);
+            addingTask(voiceInput);
+        }
+        }, {
+        indexes: ["Lösche alle Aufgaben", "Lösche", "Löschen"],
+        smart: false,  
+        action: function(): void {
+            tasks.innerHTML = "";
+            artyom.say("Alle Aufgaben wurden gelöscht");
+        }        
+        }]);
+
+    function startContinuousArtyom(): void {
+        artyom.fatality();
+        setTimeout(
+            function(): void {
+                artyom.initialize({
+                    lang: "de-DE",
+                    continuous: true,
+                    listen: true,
+                    interimResults: true,
+                    debug: true, 
+                    speed: 0.8
+                }).then(function(): void {
+                    console.log("Ready!");
+                });
+            },
+            250);
+    }
+    //Funktion "Record"-Button (Artyom hört zu/ hört auf zuzuhören) 
+    document.querySelector(".fa-microphone-alt").addEventListener("click", function(): void {
+        if (!microphoneClicked) {
+            startContinuousArtyom();
+            microphoneClicked = true; 
+        } else {
+            artyom.fatality();
+            console.log("Not ready!");
+            microphoneClicked = false;
+        }
+    });
+});
+}
